a1eed12805320b1a0a040f74a77be55b31449ec1
yusayalcin <yusayalcin6@gmail.com>
2020-05-25 17:20:42 +0300

    test

--- java-seleniumtest/src/SignUp.java
+++ java-seleniumtest/src/SignUp.java
@@ -15,7 +15,7 @@ public class SignUp {
 	public void driver() {
 
 		System.setProperty("webdriver.chrome.driver",
-				"/home/yusayalcin/eclipse-workspace/FrontEndTest/lib/chromedriver");
+				"/home/yusayalcin/git/java-seleniumtest/java-seleniumtest/lib/chromedriver");
 		driver = new ChromeDriver();
 		driver.get(baseUrl);
 	}
