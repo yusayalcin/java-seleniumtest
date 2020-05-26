a2697b7f7bf93388e3969b1f2899fd3c4eaf8e66
arzu <behiye.tarimci@ozu.edu.tr>
2020-05-26 17:56:33 +0300

    initial dosyalar
    Signed-off-by: oxcafebabe

---
+++ java-seleniumtest/src/edu/ozu/cs576/phsico/tesst/TestCaseExecutor.java
@@ -0,0 +1,10 @@
+package edu.ozu.cs576.phsico.tesst;
+
+public class TestCaseExecutor {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- java-seleniumtest/src/edu/ozu/cs576/phsico/tesst/WinMain.java
+++ java-seleniumtest/src/edu/ozu/cs576/phsico/tesst/WinMain.java
@@ -2,12 +2,13 @@ package edu.ozu.cs576.phsico.tesst;
 
 public class WinMain {
 
-	
-	public static void main (String arg [] ) {
+	public static void main(String arg[]) {
 		System.out.println("Test Otomasyon calismaya basladi.");
-		
-		
+		TestCaseExecutor executor = new TestCaseExecutor();
+
+			executor.run();
+
 		System.out.println("Test Otomasyon calismasi tamamlandi..");
-		
+
 	}
 }
